ec746d0e828542441cf7ace7fc936d6ea36b6844
vanhaohero1998 <quachvanhao2020@gmail.com>
2020-10-28 19:49:56 +0700

    new

---
+++ src/Factory/BaseArticleXFactory.php
@@ -0,0 +1,40 @@
+<?php
+namespace Societymo\Factory;
+
+use YPHP\ContainerFactoryInterface;
+use Societymo\ArticleX;
+use YPHP\FilterInputInterface;
+use YPHP\SortingInputInterface;
+
+abstract class BaseArticleXFactory implements ContainerFactoryInterface{
+
+        /**
+     * Finds an entry of the container by its identifier and returns it.
+     *
+     * @param string $id Identifier of the entry to look for.
+     * @return mixed ArticleX.
+     */
+    public abstract function get($id);
+            /**
+     * @param int $first
+     * @param string $after
+     * @param int $last
+     * @param string $before
+     * @param FilterInputInterface $filter
+     * @param SortingInputInterface $sort
+     * @return mixed
+     */
+    public abstract function list(int $first = 0,string $after = "",int $last = -1,string $before = "",FilterInputInterface $filter = null,SortingInputInterface $sort = null);
+    /**
+     * @param string $id Identifier of the entry to look for.
+     * @param ArticleX $entity
+     * @return bool
+     */
+    public abstract function update(string $id,$entity);
+        /**
+     * @param string $id Identifier of the entry to look for.
+     * 
+     * @return bool
+     */
+    public abstract function delete(string $id);
+}
